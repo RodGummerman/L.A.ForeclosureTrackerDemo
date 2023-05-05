fab284d83afa5b16fc0ab54563574983734c525d
RodGummerman <RodGummerman@gmail.com>
2023-05-05 14:02:06 -0700

    Getting a start on building the new DB and deploying it properly.

---
+++ L.A.ForeclosureTrackerDemoDB/tblProperties.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[tblProperties]
+(
+	[PropertyID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PropertyRegisteredDate] DATE NOT NULL, 
+    [PropertyStreetAddress] VARCHAR(200) NOT NULL DEFAULT '', 
+    [PropertyCity] VARCHAR(50) NOT NULL DEFAULT '', 
+    [PropertyState] CHAR(2) NOT NULL, 
+    [PropertyZipCode] VARCHAR(11) NOT NULL DEFAULT '', 
+    [PropertyTypeID] INT NOT NULL DEFAULT 0
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Date foreclosure was registered.  There should always be a value.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'tblProperties',
+    @level2type = N'COLUMN',
+    @level2name = N'PropertyRegisteredDate'
